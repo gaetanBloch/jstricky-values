9e5d5de710838d82e41bab53fcaf2ce84103b8dd
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-27 12:04:52 +0200

    Reference Values equality

--- reference-values.js
+++ reference-values.js
@@ -4,3 +4,17 @@ user.age = 32;
 console.log(student);
 
 // REFERENCE VALUES ARE MUTABLE AND ARE NOT COPIED
+
+const hobbies = ['Sports', 'Cooking'];
+// const todos = ['Sports', 'Cooking'];
+const todos = hobbies;
+
+const person = { age: 31 };
+
+console.log(hobbies == todos); // 5 == '5' => true
+console.log(hobbies === todos); // 5 === '5' => false
+
+console.log(user == person); // false
+console.log(user === person); // false
+
+// REFERENCE VALUES ARE NOT EQUAL EVEN THOUGH THE CONTENT IS THE SAME
